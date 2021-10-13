f8ad7cd0329d06b9a597943f1a1fa3257ff2e241
cyptnpassira <6305864+cyptnpassira@users.noreply.github.com>
2021-10-13 18:23:00 +0700

    build: straggler handling

--- federift/__init__.py
+++ federift/__init__.py
@@ -1,3 +1,4 @@
 """federift — educational federated-learning network & privacy simulator.
 
 This package implements a *toy* federated-learning pipeline using only the
+Python standard library. It is designed for teaching and systems-level
