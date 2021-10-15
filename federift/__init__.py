2b1e9c8a0c6f0d977af605511630b33209146c08
cyptnpassira <6305864+cyptnpassira@users.noreply.github.com>
2021-10-15 13:34:00 +0700

    fix: leakage heuristic

--- federift/__init__.py
+++ federift/__init__.py
@@ -4,3 +4,4 @@ This package implements a *toy* federated-learning pipeline using only the
 Python standard library. It is designed for teaching and systems-level
 experimentation, not for production ML and emphatically not as a privacy
 proof. See the DISCLAIMER in ``README.md``.
+
