830a1ec5c06c90f87ffbc2095b2ff0c6f1df6edc
cyptnpassira <6305864+cyptnpassira@users.noreply.github.com>
2022-01-12 11:07:00 +0700

    feat: multi-krum

---
+++ federift/aggregate.py
@@ -0,0 +1,7 @@
+"""Server-side aggregation rules.
+
+- ``fedavg``       : sample-weighted mean of client deltas (McMahan et al.).
+- ``trimmed_mean`` : coordinate-wise mean after dropping the highest/lowest
+  ``beta`` fraction per dimension. A cheap Byzantine-robust baseline.
+
+All functions take a list of client deltas and return a single aggregated
