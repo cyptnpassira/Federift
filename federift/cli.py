242ffd81d6f66bc6d377edbbc060db926ab7db12
cyptnpassira <6305864+cyptnpassira@users.noreply.github.com>
2022-04-25 18:34:00 +0700

    refactor: tidy round report

--- federift/cli.py
+++ federift/cli.py
@@ -131,3 +131,8 @@ def cmd_scenarios(_args: argparse.Namespace) -> int:
                 print(f"  {name:<28} {sc.name} "
                       f"(clients={sc.num_clients}, rounds={sc.rounds}, "
                       f"agg={sc.aggregator}, sigma={sc.sigma})")
+            except Exception as exc:  # noqa: BLE001
+                print(f"  {name:<28} <invalid: {exc}>")
+    return 0
+
+
