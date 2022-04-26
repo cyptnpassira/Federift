57fc62b666b1c0195a1b5116834567707adfa021
cyptnpassira <6305864+cyptnpassira@users.noreply.github.com>
2022-04-26 13:58:00 +0700

    fix: correct multi-krum

---
+++ federift/clients.py
@@ -0,0 +1,8 @@
+"""Deterministic toy-vector clients.
+
+A client owns a *ground-truth target vector* derived from its class-count
+signature. Local "training" is a deterministic pull of the current global
+model toward that target, plus optional per-client jitter. There is no real
+loss surface -- this is a systems/privacy teaching model, so we keep the
+dynamics analytic and reproducible.
+"""
