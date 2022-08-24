ed5940f88a20aca5ab9c68f9d7ecadae64024197
cyptnpassira <6305864+cyptnpassira@users.noreply.github.com>
2022-08-24 10:21:00 +0700

    feat: straggler handling

---
+++ federift/partition.py
@@ -0,0 +1,11 @@
+"""Data partitioning for federated clients.
+
+The simulator uses a *toy classification* setup: each sample is a label in
+``range(num_classes)``. We do not carry real features -- a client's "data" is
+summarised by its per-class counts, which is enough to (a) drive a
+deterministic local target vector and (b) study non-IID skew.
+
+Two partitioners are provided:
+
+- ``iid_partition``       : shuffle and split evenly.
+- ``dirichlet_partition`` : Dirichlet(alpha) label skew, the standard knob for
