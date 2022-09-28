66d434e987800fb232d5f9980168bc0edab8d25b
cyptnpassira <6305864+cyptnpassira@users.noreply.github.com>
2022-09-28 14:34:00 +0700

    test: cover advanced composition

--- federift/privacy.py
+++ federift/privacy.py
@@ -9,3 +9,8 @@ moments accountant). Do not cite numbers from this module as a privacy
 guarantee for anything real. See the DISCLAIMER in the README.
 
 We model the standard DP-SGD-flavoured pipeline:
+
+  1. clip each client delta to L2 norm C,
+  2. add Gaussian noise N(0, (sigma*C)^2 * I) at the server,
+  3. account for the (epsilon, delta) budget across rounds.
+"""
