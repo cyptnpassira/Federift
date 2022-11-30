ff1f8e5b7a7128f9bcdb4209dcb05ebd442bebbf
cyptnpassira <6305864+cyptnpassira@users.noreply.github.com>
2022-11-30 15:03:00 +0700

    perf: improve examples

--- federift/rng.py
+++ federift/rng.py
@@ -1,2 +1,4 @@
 """Deterministic random helpers.
 
+We wrap :class:`random.Random` so every stochastic step in the simulator is
+seeded and reproducible. A single master seed fans out into per-client and
