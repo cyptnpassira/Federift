e73eb1dc22da215791564e049dad3c786ce89b45
cyptnpassira <6305864+cyptnpassira@users.noreply.github.com>
2022-12-02 11:27:00 +0700

    chore: multi-krum

--- federift/rng.py
+++ federift/rng.py
@@ -2,3 +2,4 @@
 
 We wrap :class:`random.Random` so every stochastic step in the simulator is
 seeded and reproducible. A single master seed fans out into per-client and
+per-round sub-streams via stable string keys.
