3f3bc1ff20b65fa8bcddd6ba12cfa559459af1f2
cyptnpassira <6305864+cyptnpassira@users.noreply.github.com>
2022-12-02 11:35:00 +0700

    fix: correct privacy report

--- federift/rng.py
+++ federift/rng.py
@@ -3,3 +3,4 @@
 We wrap :class:`random.Random` so every stochastic step in the simulator is
 seeded and reproducible. A single master seed fans out into per-client and
 per-round sub-streams via stable string keys.
+"""
