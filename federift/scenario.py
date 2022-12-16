a10c391cc7e04d92b4a4f9535bf0344d0ebdf821
cyptnpassira <6305864+cyptnpassira@users.noreply.github.com>
2022-12-16 16:03:00 +0700

    perf: improve fedavg aggregation

---
+++ federift/scenario.py
@@ -0,0 +1,10 @@
+"""Shared JSON scenario schema.
+
+A *scenario* is the single source of truth shared between the Python privacy
+core and the Go topology engine. Both read the same file. The Python side uses
+the ``federation`` + ``privacy`` blocks; the Go engine uses the ``network``
+block. Fields it doesn't recognise are ignored on each side, so the two tools
+stay loosely coupled.
+"""
+
+from __future__ import annotations
