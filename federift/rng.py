954f3ef06d6388e351300fb29650741805fd34f4
cyptnpassira <6305864+cyptnpassira@users.noreply.github.com>
2022-12-16 15:22:00 +0700

    refactor: tidy multi-krum

--- federift/rng.py
+++ federift/rng.py
@@ -17,3 +17,4 @@ def derive_seed(master: int, *parts: object) -> int:
     key = "::".join([str(master)] + [str(p) for p in parts])
     digest = hashlib.sha256(key.encode("utf-8")).digest()
     return int.from_bytes(digest[:8], "big") & 0x7FFFFFFFFFFFFFFF
+
