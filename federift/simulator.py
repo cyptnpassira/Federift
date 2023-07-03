358d432443e82d2bb5a2289e3062d28ced16ad4a
cyptnpassira <6305864+cyptnpassira@users.noreply.github.com>
2023-07-03 14:27:00 +0700

    refactor: tidy iid partition

---
+++ federift/simulator.py
@@ -0,0 +1,17 @@
+"""The federated round loop.
+
+Each round:
+
+  1. Select a subset of clients (deterministic per round).
+  2. Each selected client computes a local delta toward its target.
+  3. Deltas are L2-clipped to ``clip_norm``.
+  4. The server aggregates (FedAvg / trimmed / krum).
+  5. Gaussian DP noise is optionally added to the aggregate.
+  6. The global model steps by the (noised) aggregate.
+
+The simulator can optionally consume a Go-produced *network trace* to drop
+stragglers/partitioned clients from a round -- coupling the privacy core to
+the topology engine through the shared scenario.
+"""
+
+from __future__ import annotations
